feac1af75f80927be1a2817cbde535f8ececd2bb
PunkinPunkin <PunkinPunkin@livemail.tw>
2020-02-03 22:52:25 +0800

    Home

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import Vue from './../node_modules/vue'
+import { BootstrapVue, IconsPlugin } from './../node_modules/bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import './../node_modules/bootstrap/dist/css/bootstrap.css'
+import './../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
